286b83680523ea4cf6c0651084e25d2bf9e18354
Maanas Kejriwal <83580613+MaanasKejriwal@users.noreply.github.com>
2022-10-25 15:58:04 +0530

    Create LinkedListImplementation.c

---
+++ ClassroomDeliverables/LinkedListImplementation.c
@@ -0,0 +1,24 @@
+//Implementation of a Linked List: Given a set of values (assume int), create a linked list from them
+
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int val;
+    struct node *next;
+};
+int main(){
+    struct node *head = NULL;
+    struct node *temp = NULL;
+    int val = scanf("%d", &val);
+    while (val > 0){
+        temp = malloc(sizeof(struct node));
+        temp->val = val;
+        temp->next = head;
+        head = temp;
+        val = scanf("%d", &val);
+        free(temp);
+    }
+    struct node *c;
+    for (c = head; c != NULL; c = c->next){
+        printf("%d", c->val);}
+}
