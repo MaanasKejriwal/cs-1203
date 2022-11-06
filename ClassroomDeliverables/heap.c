520965d7e30c259a8f8960e4dbef559119384436
Maanas Kejriwal <83580613+MaanasKejriwal@users.noreply.github.com>
2022-11-06 23:05:12 +0530

    Update heap.c
    Added comments

--- ClassroomDeliverables/heap.c
+++ ClassroomDeliverables/heap.c
@@ -1,72 +1,67 @@
 #include<stdio.h>
 #include<limits.h>
- 
-/*Declaring heap globally so that we do not need to pass it as an argument every time*/
-/* Heap implemented  here is Min Heap */
- 
-int heap[1000000], heapSize;
-/*Initialize Heap*/
+
+/* Heap declared globally */
+
+int heap[1000000], size;
+
 void Init() {
-    heapSize = 0;
+    size = 0;
     heap[0] = -INT_MAX;
 }
- 
-/*Insert an element into the heap */
-void Insert(int element) {
-    heapSize++;
-    heap[heapSize] = element; /*Insert in the last place*/
-    /*Adjust its position*/
-    int now = heapSize;
-    while (heap[now / 2] > element) {
-        heap[now] = heap[now / 2];
-        now /= 2;
+
+/*To insert heap elem */
+void Insert(int elem) {
+    size++;
+    heap[size] = elem; /*Insert in the -1 position*/
+    int current = size;
+    while (heap[current / 2] > elem) {
+        heap[current] = heap[current / 2];
+        current /= 2;
     }
-    heap[now] = element;
+    heap[current] = elem;
 }
- 
+
 int DeleteMin() {
-    /* heap[1] is the minimum element. So we remove heap[1]. Size of the heap is decreased.
-     Now heap[1] has to be filled. We put the last element in its place and see if it fits.
-     If it does not fit, take minimum element among both its children and replaces parent with it.
-     Again See if the last element fits in that place.*/
-    int minElement, lastElement, child, now;
+    /* Since heap[1] is the minimum elem, it is removed. We put the last elem in its place and see if it fits.
+     If it doesn't fit, take min elem among its children replacing parent with it.
+     Process repeats.*/
+    int minElement, lastElement, child, current;
     minElement = heap[1];
-    lastElement = heap[heapSize--];
-    /* now refers to the index at which we are now */
-    for (now = 1; now * 2 <= heapSize; now = child) {
-        /* child is the index of the element which is minimum among both the children */
+    lastElement = heap[size--];
+    /* index counter */
+    for (current = 1; current * 2 <= size; current = child) {
         /* Indexes of children are i*2 and i*2 + 1*/
-        child = now * 2;
-        /*child!=heapSize beacuse heap[heapSize+1] does not exist, which means it has only one
-         child */
-        if (child != heapSize && heap[child + 1] < heap[child]) {
+        child = current * 2;
+        /*child is not size because heap[size+1] doesn't exist, (has only one
+         child) */
+        if (child != size && heap[child + 1] < heap[child]) {
             child++;
         }
-        /* To check if the last element fits ot not it suffices to check if the last element
-         is less than the minimum element among both the children*/
+
         if (lastElement > heap[child]) {
-            heap[now] = heap[child];
-        } else /* It fits there */
+            heap[current] = heap[child];
+        } else /* Fits */
         {
             break;
         }
     }
-    heap[now] = lastElement;
+    heap[current] = lastElement;
     return minElement;
 }
- 
+
 int main() {
-    int number_of_elements;
+    int elemnum;
     printf("Program to demonstrate Heap:\nEnter the number of elements: ");
-    scanf("%d", &number_of_elements);
-    int iter, element;
+    scanf("%d", &elemnum);
+    int i, elem;
     Init();
     printf("Enter the elements: ");
-    for (iter = 0; iter < number_of_elements; iter++) {
-        scanf("%d", &element);
-        Insert(element);
+    for (i = 0; i < elemnum; i++) {
+        scanf("%d", &elem);
+        Insert(elem);
     }
-    for (iter = 0; iter < number_of_elements; iter++) {
+    for (i = 0; i < elemnum; i++) {
         printf("%d ", DeleteMin());
     }
     printf("\n");
