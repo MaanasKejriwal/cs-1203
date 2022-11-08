d17edbfd69b60cb1bb1813a8c191bd052376a993
Maanas Kejriwal <83580613+MaanasKejriwal@users.noreply.github.com>
2022-11-08 23:25:00 +0530

    Update LinkedListImplementation.c
    pareshaan karke rakha hai econ majors ne

--- ClassroomDeliverables/LinkedListImplementation.c
+++ ClassroomDeliverables/LinkedListImplementation.c
@@ -1,88 +1,68 @@
-//Implementation of a Linked List: Given a set of values (assume int), create a linked list from them
+//
+// Created by Maanas on 11/8/2022.
+//
+#include "stdio.h"
+#include "stdlib.h"
 
-#include <stdio.h>
-#include <stdlib.h>
+typedef struct node{        //defining the structure of a node, and calling it a node
+    int value;              //actual value in the node
+    struct node *nextadd;   //pointer to address of next node
+}node;                      //referential
 
-struct node
-{
-    int number;
-    struct node *next;
-}*startnode;
+node * creation(int len);
 
-void listPrint()
-{
-    struct node *chkr;
-    if(startnode == NULL)
-    {
-        printf("Empty List!"); //error if empty list
-    }
-    else
-    {
-        chkr = startnode;
-        while(chkr != NULL)
-        {
-            printf(" Value : %d\n", chkr->number);  //ask for the value of the node
-            chkr = chkr->next;
-        }
-    }
-}
-
-void linkedList(int n)
-{
-    struct node *fnNode, *chkr;
-    int number, i;
-    startnode = (struct node *)malloc(sizeof(struct node));
-
-    if(startnode == NULL)
-    {
-        printf("Node is NULL");
-    }
-    else
-    {
+void listdisplay(node * head);
 
 
-        printf("Input value for node 1 : ");
-        scanf("%d", &number);
-        startnode->number = number;
-        startnode->next = NULL;
-        chkr = startnode;
-
-        for(i=2; i<=n; i++)
-        {
-            fnNode = (struct node *)malloc(sizeof(struct node));
-            if(fnNode == NULL)
-            {
-                printf("Memory is NULL");
-                break;
-            }
-            else
-            {
-                printf("Enter value for node %d : ", i);
-                scanf(" %d", &number);
-
-                fnNode->number = number;
-                fnNode->next = NULL;
-
-                chkr->next = fnNode;
-                chkr = chkr->next;
-            }
-        }
-    }
-}
+int main(){                                             //for len of node
+    int len = 0;                                        //initialise length as 0
+    node * HEAD = NULL;                                 //first node value initialised
+    printf("Enter number of nodes: \n");        //input for number of nodes
+    scanf("%d", &len);
+    HEAD = creation(len);
+    listdisplay(HEAD);
+    return 0;
 
 
 
-void linkedList(int n); 
-void listPrint();         
-
-int main()
-{
-    int n;
-
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
-    linkedList(n);
-    printf("\n Here is your Linked List : : \n");
-    listPrint();
     return 0;
 }
+
+node * creation(int len){
+    int a = 0;
+    node * head = NULL;
+    node * temp = NULL;
+    node * p = NULL;
+
+    for(a=0; a< len; a++)
+    {
+        temp = (node*)malloc(sizeof(node));
+        printf("\nEnter data for node: ", a+1);
+        scanf("%d",&(temp->value));
+        temp->nextadd = NULL;
+
+        if(head == NULL)
+        {
+            head = temp;
+        }
+        else{
+            p = head;
+            while(p->nextadd !=NULL)
+                p = p->nextadd;
+            p-> nextadd= temp;
+
+        }
+
+    }
+    return head;
+}
+
+void listdisplay(node * head){
+    node * p = head;
+
+    while(p != NULL) {
+        printf("\t%d->", p->value);
+        p = p->nextadd;
+    }
+    }
+
