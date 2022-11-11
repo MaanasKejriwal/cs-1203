cd0c6de736842c697dcf43b3507040d893aeb511
Maanas Kejriwal <83580613+MaanasKejriwal@users.noreply.github.com>
2022-11-11 11:26:14 +0530

    Update heap.c

--- ClassroomDeliverables/heap.c
+++ ClassroomDeliverables/heap.c
@@ -1,71 +1,55 @@
-#include<stdio.h>
-#include<limits.h>
+// Given an array, build a heap and sort it!
 
-/* Heap declared globally */
+#include <stdio.h>
 
-int heap[1000000], size;
+void createHeap(int arr[], int len, int a) {            //function to create heap
+    int greatestElem = a;                               //find largest element
+    int leftChild = 2 * a + 1;                          //checking left child
+    int rightChild = 2 * a + 2;                         //checking right child
 
-void Init() {
-    size = 0;
-    heap[0] = -INT_MAX;
+    if (leftChild < len && arr[leftChild] > arr[greatestElem])      //if leftchild is the greatest element condition
+        greatestElem = leftChild;                                   //setting greatest element as left child
+
+    if (rightChild < len && arr[rightChild] > arr[greatestElem])    //if rightchild is the greatest element condition
+        greatestElem = rightChild;                                  //setting greatest element as right child
+
+    if (greatestElem != a) {                                        //swap until a is not the greatest element
+        int t = arr[a];                                             //swapping using a temp variable
+        arr[a] = arr[greatestElem];
+        arr[greatestElem] = t;
+        createHeap(arr, len, greatestElem);                         //heap creation
+    }
 }
 
-/*To insert heap elem */
-void Insert(int elem) {
-    size++;
-    heap[size] = elem; /*Insert in the -1 position*/
-    int current = size;
-    while (heap[current / 2] > elem) {
-        heap[current] = heap[current / 2];
-        current /= 2;
+void sortMain(int arr[], int len) {                                 //main sorting function
+    for (int a = len / 2 - 1; a >= 0; a--)
+        createHeap(arr, len, a);                                    //heap creation
+
+    for (int a = len - 1; a >= 0; a--) {                            //actual heap sort
+        int t = arr[0];                                             //swapping elements
+        arr[0] = arr[a];
+        arr[a] = t;
+
+        createHeap(arr, a, 0);                                  //heapification to get a
     }
-    heap[current] = elem;
 }
 
-int DeleteMin() {
-    /* Since heap[1] is the minimum elem, it is removed. We put the last elem in its place and see if it fits.
-     If it doesn't fit, take min elem among its children replacing parent with it.
-     Process repeats.*/
-    int minElement, lastElement, child, current;
-    minElement = heap[1];
-    lastElement = heap[size--];
-    /* index counter */
-    for (current = 1; current * 2 <= size; current = child) {
-        /* Indexes of children are i*2 and i*2 + 1*/
-        child = current * 2;
-        /*child is not size because heap[size+1] doesn't exist, (has only one
-         child) */
-        if (child != size && heap[child + 1] < heap[child]) {
-            child++;
-        }
-
-        if (lastElement > heap[child]) {
-            heap[current] = heap[child];
-        } else /* Fits */
-        {
-            break;
-        }
-    }
-    heap[current] = lastElement;
-    return minElement;
-}
-
-int main() {
-    int elemnum;
-    printf("Program to demonstrate Heap:\nEnter the number of elements: ");
-    scanf("%d", &elemnum);
-    int i, elem;
-    Init();
-    printf("Enter the elements: ");
-    for (i = 0; i < elemnum; i++) {
-        scanf("%d", &elem);
-        Insert(elem);
-    }
-    for (i = 0; i < elemnum; i++) {
-        printf("%d ", DeleteMin());
-    }
+void ArrayDisplay(int arr[], int len) {                             //function to display array
+    for (int a = 0; a < len; ++a)
+        printf("%d ", arr[a]);
     printf("\n");
-    return 0;
 }
 
-//reference: https://www.sanfoundry.com/c-program-implement-heap/
+
+int main() {                                                        //main function
+    int data[] = {75, 24, 53, 86, 12, 43, 11, 0};                   //original array is pre-defined as per instructions, could be any random array
+    int len = sizeof(data) / sizeof(data[0]);
+
+    printf("Original Array: ");                                 //displaying original array for reference pre-sorting
+    ArrayDisplay(data, len);
+
+    sortMain(data, len);                                                //sorting
+
+    printf("Array after sorting: ");
+    ArrayDisplay(data, len);                                            //displaying sorted array
+}
